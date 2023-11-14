b24c0e7700315dbe9faa6ad1372a6593f7b6c848
ajerryyang <59729276+ajerryyang@users.noreply.github.com>
2023-11-15 00:08:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -44,6 +44,35 @@ def handle_message(event):
          line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text='已通知物流來領取'))
+
+    elif message_text == 'test':
+        message = TemplateSendMessage(
+        alt_text='Buttons template',
+        template=ButtonsTemplate(
+            thumbnail_image_url='https://example.com/image.jpg',
+            title='Menu',
+            text='Please select',
+            actions=[
+                PostbackTemplateAction(
+                    label='postback',
+                    text='postback text',
+                    data='action=buy&itemid=1'
+                ),
+                MessageTemplateAction(
+                    label='message',
+                    text='message text'
+                ),
+                URITemplateAction(
+                    label='uri',
+                    uri='http://example.com/'
+                )
+                ]
+            )
+        )
+        line_bot_api.reply_message(event.reply_token, message)
+
+
+    
     else:
         line_bot_api.reply_message(
             event.reply_token,
